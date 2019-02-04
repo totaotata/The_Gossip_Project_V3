c06af3c3ad93a459126e3262d4fab95b67dd6fc2
totaotata <totaotata@protonmail.com>
2019-02-04 18:01:23 +0100

    OKKKKKK

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get '/user/:id', to: 'user#show'
+  get '/welcome', to: 'welcome#show'
+  get '/gossip/:id', to: 'gossip#show'
+  get '/', to: 'welcome#show'
   get '/welcome/:first_name', to: 'welcome#show'
   get '/team', to: 'team#show'
   get '/contact', to: 'contact#show'
