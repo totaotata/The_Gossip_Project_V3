d27ac82d73be61fb4a6308cba599330d1c260311
totaotata <totaotata@protonmail.com>
2019-02-05 20:09:27 +0100

    Create Gossip OK

--- app/controllers/gossip_controller.rb
+++ app/controllers/gossip_controller.rb
@@ -2,6 +2,22 @@
 class GossipController < ApplicationController
   def show
     first_occurence= Gossip.first.id
-    @gossip = Gossip.find( first_occurence + params[:id].to_i)
+    @gossip = Gossip.find( first_occurence + params[:id].to_i + 1)
   end
+  def create
+  @user = User.find(21)
+  @gossip = Gossip.new('user_id' => @user.id,
+                        'title' => params[:title],
+                        'content' => params[:content])
+if @gossip.save # essaie de sauvegarder en base @gossip
+    # si ça marche, il redirige vers la page d'index du site
+    redirect_to welcome_path
+   else
+  #   # sinon, il render la view new (qui est celle sur laquelle on est déjà)
+     render new_gossip_path
+   end
+end
+def new
+  @gossip = Gossip.new
+end
 end
